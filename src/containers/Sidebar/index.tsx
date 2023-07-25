cbe1ed73c3f4466eadc42b598cbe2845090497e4
luanafernanda <lfsg.22@outlook.com>
2023-07-25 13:12:21 -0300

    constuindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/luanafernanda.png" alt="" />
+    <Titulo fontSize={20}>Luana Fernanda</Titulo>
+  </aside>
+)
+
+export default Sidebar
