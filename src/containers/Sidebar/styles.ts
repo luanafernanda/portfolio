f25923518a3f8fe308430902602ed1bf11c13259
luanafernanda <lfsg.22@outlook.com>
2023-07-25 15:51:01 -0300

    estilizando a sidebar

---
+++ src/containers/Sidebar/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+import { P } from '../../components/Paragrafo/styles'
+
+export const Descricao = styled(P)`
+  margin-top: 34px;
+  margin-bottom: 40px;
+`
+
+export const BotaoTema = styled.button`
+  padding: 8px;
+  border-radius: 12px:
+  color: #eee;
+  font-size: 10px;
+  font-wight: bold;
+  background-color: #282A35;
+  cursor: pointer;  
+`
+export const SidebarContainer = styled.div`
+  position: sticky;
+  top: 80px;
+  left: 0;
+`
